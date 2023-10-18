75e254af69deba098054588498c29b2c8f211e75
MonoDG <monodg@proton.me>
2023-10-18 00:09:33 -0400

    Add active style to nav buttons

--- src/index.js
+++ src/index.js
@@ -7,16 +7,24 @@ pageLoad();
 
 const btnHome = document.querySelector("#btn-home");
 const btnMenu = document.querySelector("#btn-menu");
-const btnContent = document.querySelector("#btn-content");
+const btnContact = document.querySelector("#btn-contact");
 const mainContent = document.querySelector(".main-content");
 
+btnHome.classList.add("btn-active");
+
 btnHome.addEventListener("click", () => {
+    btnMenu.classList.remove("btn-active");
+    btnContact.classList.remove("btn-active");
+    btnHome.classList.add("btn-active");
     mainContent.replaceChildren();
     mainContent.appendChild(createHome());
     mainContent.appendChild(createFooter());
 });
 
 btnMenu.addEventListener("click", () => {
+    btnHome.classList.remove("btn-active");
+    btnContact.classList.remove("btn-active");
+    btnMenu.classList.add("btn-active");
     mainContent.replaceChildren();
     mainContent.appendChild(createMenu());
     mainContent.appendChild(createFooter());
