b984f0c5f8bd3b05d606c0e3d0709aa001780b1c
MonoDG <monodg@proton.me>
2023-10-17 23:59:35 -0400

    Add home and menu button event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import pageLoad from "./pageLoad";
+import createHome from "./home";
+import createMenu from "./menu";
+import createFooter from "./footer";
 
 pageLoad();
+
+const btnHome = document.querySelector("#btn-home");
+const btnMenu = document.querySelector("#btn-menu");
+const btnContent = document.querySelector("#btn-content");
+const mainContent = document.querySelector(".main-content");
+
+btnHome.addEventListener("click", () => {
+    mainContent.replaceChildren();
+    mainContent.appendChild(createHome());
+    mainContent.appendChild(createFooter());
+});
+
+btnMenu.addEventListener("click", () => {
+    mainContent.replaceChildren();
+    mainContent.appendChild(createMenu());
+    mainContent.appendChild(createFooter());
+});
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+export default function createMenu() {
+    const menu = document.createElement("section");
+    menu.textContent = "Menu Section";
+
+    return menu;
+}
